60da7f3ff8f7a15545b225ae330dc09bfb9cffa6
Ryan Yang <backyardshark@Gmail.com>
2021-03-01 19:44:00 -0500

    removed ocr due to its inaccuracy

--- bombparty-bot.py
+++ bombparty-bot.py
@@ -2,31 +2,50 @@ import pynput
 from pynput import keyboard
 import os
 import time
-from pynput.keyboard import HotKey, Key, Controller, Listener
-import PIL
-from PIL import ImageGrab
+from pynput.keyboard import Key, Controller as KeyboardController
+from pynput.mouse import Button, Controller as MouseController
+# import PIL
+# from PIL import ImageGrab
 import io
-from PIL import Image
-import pytesseract
+# from PIL import Image
 import random
+import pyperclip
 
-pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract'
-
-filepath = r'C:\Users\zZDOU\Downloads\bombparty ruiner\capture.png'
-
-send = Controller()
+send = KeyboardController()
+mouse = MouseController()
 
 with open(r"C:\Users\zZDOU\Downloads\bombparty ruiner\wordlist.txt", encoding="utf8") as f:
     content = f.readlines()
 
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+usedletters = []
+atoz = []
+
 def on_activate():
-    screenshot = ImageGrab.grab(bbox=(780, 540, 840, 570))
-    screenshot.save(filepath, 'PNG')
-    try:
-        result = pytesseract.image_to_string(Image.open(r"C:\Users\zZDOU\Downloads\bombparty ruiner\capture.png"), timeout=3)
-    except RuntimeError as timeout_error:
-        print("timeout")
-        pass
+    pos = mouse.position
+    mouse.position = (810, 555)
+    mouse.press(Button.left)
+    mouse.release(Button.left)
+    mouse.press(Button.left)
+    mouse.release(Button.left)
+    send.press(Key.ctrl)
+    send.press('c')
+    time.sleep(0.01)
+    send.release('c')
+    send.release(Key.ctrl)
+    
+    time.sleep(0.1)
+
+    result = pyperclip.paste()
+    time.sleep(0.01)
+    pyperclip.copy('')
+
+    mouse.position = (pos)
+    mouse.press(Button.left)
+    mouse.release(Button.left)
+    mouse.press(Button.left)
+    mouse.release(Button.left)
     
     search = result.lower()
     search = search.strip()
@@ -40,16 +59,23 @@ def on_activate():
     if (arrsize == 0):
         return print("error")
 
-    rand = random.randint(0, arrsize-1)
-    for char in found[rand]:
+    choice = random.randint(0, arrsize-1)
+    
+    for char in found[choice]:
+        usedletters.append(char)
         send.press(char)
         send.release(char)
-        time.sleep(0.0001)
-    time.sleep(0.04)
+        time.sleep(0.096)
+    nodups = list(dict.fromkeys(usedletters))
+    atoz = sorted(nodups)
+    time.sleep(0.02)
     send.press(Key.enter)
-    time.sleep(0.001)
     send.release(Key.enter)
             
+def reset_bonus():
+    usedletters = []
+    nodups = []
+    atoz = []
 
 def for_canonical(f):
     return lambda k: f(l.canonical(k))
